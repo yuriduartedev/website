bf24a3237c5d2671d967f498d870750f73f91138
Yuri Duarte <yuriduartedev@gmail.com>
2021-06-24 10:39:58 -0300

    Adiciona tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext
 } from 'next/document'
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -38,7 +39,24 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head />
+        <meta charSet="utf-8" />
+
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TDFFSWQ');`
+            }}
+          ></script>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDFFSWQ"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
           <Main />
           <NextScript />
         </body>
