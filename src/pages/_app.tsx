323ec03e5ee62bbd76b3c02a83d45d88aacc1406
Yuri Duarte <yuriduartedev@gmail.com>
2022-02-23 18:26:44 -0400

    test remove manifest json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-
         <link rel="shortcut icon" href="/img/dedos_cruzados.png" />
         <link rel="apple-touch-icon" href="/img/dedos_cruzados.png" />
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
-
 
         <title>Apartamento à venda em Botafogo com 2 quartos - Campainha</title>
         <meta name="description" content="Em 1931, Winston Churchill previu que um dia o homem escaparia do absurdo de criar uma galinha inteira para comer somente o peito ou a asa, produzindo essas partes separadamente em um meio adequado." />
